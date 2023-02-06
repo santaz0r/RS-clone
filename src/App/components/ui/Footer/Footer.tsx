f433aefd20ec67f96b9d0eefa0fc9ca185450555
VictorM <neuron96@mail.ru>
2023-02-06 18:13:53 +0500

    feat: add some styles to footer

--- src/App/components/ui/Footer/Footer.tsx
+++ src/App/components/ui/Footer/Footer.tsx
@@ -1,5 +1,37 @@
+import styles from './Footer.module.scss';
+import githubLogo from '../../../assets/github-mark-white.png';
+
 function Footer() {
-  return <footer>some footer</footer>;
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footer__blackbar_wrapper}>
+        <ul className={styles.footer__blackbar}>
+          <li className={styles.github__links}>
+            <a className={styles.github__link} href="//github.com/santaz0r">
+              <img className={styles.github__logo} src={githubLogo} alt="github logo" />
+              <span>santaz0r</span>
+            </a>
+            <a className={styles.github__link} href="//github.com/Te1epuz">
+              <img className={styles.github__logo} src={githubLogo} alt="github logo" />
+              <span>te1epuz</span>
+            </a>
+            <a className={styles.github__link} href="//github.com/mordobiv">
+              <img className={styles.github__logo} src={githubLogo} alt="github logo" />
+              <span>mordobiv</span>
+            </a>
+          </li>
+          <li>
+            <span>2023</span>
+          </li>
+          <li>
+            <a className={styles.rsschool__link} href="https://rs.school/js/">
+              <span>Rs School</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
