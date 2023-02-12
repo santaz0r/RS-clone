1e970d2c66b48ef7af0ce383fd52f6597510ad51
Alexander Martinovich <alexander_martinovich@msn.com>
2023-02-12 23:36:44 +0300

    feat: add contacts routes and empty page

---
+++ src/App/pages/Contacts/Contacts.tsx
@@ -0,0 +1,11 @@
+import styles from './Contacts.module.scss';
+
+function Contacts() {
+  return (
+    <div className={styles.contacts__wrapper}>
+      Contacts
+    </div>
+  );
+}
+
+export default Contacts;
